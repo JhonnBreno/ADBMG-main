ec192b863959d6c95642db3a1e12d5b4a82baeb7
Jhonn Breno <jhonnbreno345@gmail.com>
2025-02-25 23:36:47 -0300

    25/02/2025

--- locais/locais.js
+++ locais/locais.js
@@ -2,7 +2,7 @@ function search(){
     let input = document.getElementById('searchbar').value
     input = input.toLowerCase()
     let local = document.getElementsByClassName('locais')
-    
+
     for (i = 0; i < local.length; i++){
         if(!local[i].innerHTML.toLowerCase().includes(input)){
             local[i].style.display = "none"
